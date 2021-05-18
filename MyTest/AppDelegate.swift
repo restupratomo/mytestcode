80017df4082ce2bf70f47528cd8a9904eba5c990
restupratomo <res_17_ok@yahoo.com>
2021-05-18 09:52:27 +0700

    Inital commit
    My Test initial commit

---
+++ MyTest/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  InventoryTest
+//
+//  Created by Restu Suryo Pratomo on 14/05/21.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        let windowFrame: CGRect = UIScreen.main.bounds
+        self.window = UIWindow(frame: windowFrame)
+        let nav:UINavigationController = UINavigationController(rootViewController: LoginViewController())
+        self.window?.rootViewController = nav
+        self.window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+}
+
---
+++ MyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InventoryTest
+//
+//  Created by Restu Suryo Pratomo on 14/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
